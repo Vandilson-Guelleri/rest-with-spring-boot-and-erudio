39801b75a720795c534269a946054988a7e51817
Vandilson Guelleri <vandilsonguelleri@gmail.com>
2024-09-06 14:13:35 -0300

    Criando Exceptions Customizadas e o VO de Credenciais

--- src/main/java/br/com/erudio/exceptions/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/br/com/erudio/exceptions/CustomizedResponseEntityExceptionHandler.java
@@ -43,4 +43,14 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 				request.getDescription(false));
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
 	}
+	
+	@ExceptionHandler(InvalidJwtAuthenticationException.class)
+	public final ResponseEntity<ExceptionResponse> handInvalidJwtAuthenticationException(Exception ex, WebRequest request){
+		
+		ExceptionResponse exceptionResponse = new ExceptionResponse(
+				new Date(), 
+				ex.getMessage(), 
+				request.getDescription(false));
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.FORBIDDEN);
+	}
 }
